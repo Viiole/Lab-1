106a2e66a8fab413119e51e2ea40c3c6bfb4bfe9
Ekaterina Tsygankova <cigankova2002@mail.ru>
2022-02-17 22:02:22 +0700

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+// В ПРОЦЕССЕ
+namespace Lab1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            int[] firstСheckout = { 5, 3, 4 };
+            Console.WriteLine("HW1. QueueTime([5, 3, 4], 1) Время обслуживания всех покупателей: " + HW1.QueueTime(firstСheckout, 1));
+
+            int[] secondСheckout = { 10, 2, 3, 3 };
+            Console.WriteLine("HW1. QueueTime([10, 2, 3, 3], 2) Время обслуживания всех покупателей: " + HW1.QueueTime(secondСheckout, 2));
+
+            int[] thirdСheckout = { 2, 3, 10 };
+            Console.WriteLine("HW1. QueueTime([2, 3, 10], 2) Время обслуживания всех покупателей: " + HW1.QueueTime(thirdСheckout, 2));
+        } 
+    }
+    public class HW1
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            int[] threadsPool = new int[n];
+            int time = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+                threadsPool[j] = customers[j];
+            }
+
+            int check = n;
+
+            while (check < customers.Length)
+            {
+                int minBuyerTime = threadsPool[0];
+
+                for (int j = 1; j < n; j++)
+                {
+                    if (threadsPool[j] < minBuyerTime)
+                    {
+                        minBuyerTime = threadsPool[j];
+                    }
+                }
+
+                for (int j = 0; j < n; j++)
+                {
+                    threadsPool[j] -= minBuyerTime;
+                }
+
+                time += minBuyerTime;
+            }
+
+            if (customers.Length < n)
+            {
+                n = customers.Length;
+            }
+
+            return time;
+        }
+    }
+}
